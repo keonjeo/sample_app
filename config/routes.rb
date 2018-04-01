64f34de12bda38efab61f07172bb5ada66ccdff6
Keon Ye <jnuyeyizhou@gmail.com>
2018-04-01 22:33:34 +0800

    add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   # root 'application#hello'
   root 'static_pages#home'
 
@@ -6,5 +8,6 @@ Rails.application.routes.draw do
   get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
+  get '/signup', to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
