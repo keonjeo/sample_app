f58e5dc3961643403e8db474f366429457f6ffaa
Keon Ye <jnuyeyizhou@gmail.com>
2018-04-02 23:31:31 +0800

    add user sign up module

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
+
   def new
+    @user = User.new
   end
+
+  def show
+    @user = User.find(params[:id])
+    # debugger
+  end
+
+  def create
+
+    @user = User.new(user_params)
+    if @user.save
+      # 处理注册成功的情况
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      flash[:alert] = "Sorry! Failed to create a new account!"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
